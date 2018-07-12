2dbfd2eabeac25759c7dc74b35c105a79a263a30
chris nguyen <chuonchuonit@gmail.com>
2018-07-12 08:34:25 +0700

    update migate script

--- src/migrations/2_initial_bbodigitalcontract.js
+++ src/migrations/2_initial_bbodigitalcontract.js
@@ -1,5 +1,17 @@
-var BigbomDigitalContract = artifacts.require("./BigbomDigitalContract.sol");
+const DigitalContract =  artifacts.require("BigbomDigitalContract");
+const BBStorage =  artifacts.require("BBStorage");
+
 
 module.exports = function(deployer) {
-   deployer.deploy(BigbomDigitalContract);
+   var storageaddress;
+   return BBStorage.new().then(function(storage){
+   	 storageaddress = storage.address;
+   	 return DigitalContract.new();
+   }).then(function(instance){
+   	console.log('address', storageaddress)
+   	 return instance.setStorage(storageaddress);
+   }).then(function(rs){
+   	 console.log(rs);
+   	 process.exit(1);
+   });
 };
