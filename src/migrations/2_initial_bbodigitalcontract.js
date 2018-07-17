9f57126ed1a37485d658acd7e5025b1fcdda7682
Chris TN <ksmanhtuong103@gmail.com>
2018-07-17 10:42:40 +0700

    fix deploy issue

--- src/migrations/2_initial_bbodigitalcontract.js
+++ src/migrations/2_initial_bbodigitalcontract.js
@@ -4,46 +4,30 @@ const ProxyFactory = artifacts.require("UpgradeabilityProxyFactory");
 
 
 module.exports = function(deployer) {
-   var storageaddress;
-   var storageinstance;
-   var dinstance;
-   var proxyaddress;
    
-   return BBStorage.new().then(function(storage){
-   	 storageaddress = storage.address;
-       storageinstance = storage;
-   	 return DigitalContract.new();
-   }).then(function(instance){
-   	console.log('storage address', storageaddress)
-      console.log('digital address', instance.address)
-      dinstance = instance;
-   	
-      return ProxyFactory.new();
-   }).then(function(proxyFactory){
-      console.log('proxyFactory ', proxyFactory.address);
+   var storageinstance ;
+   var dinstance;
+   var proxyFactory ;
+   var proxyAddress;
+   deployer.deploy(BBStorage).then(function(rs){
+      storageinstance = rs;
+      return deployer.deploy(DigitalContract);
+   }).then(function(rs){
+      dinstance = rs;
+      return deployer.deploy(ProxyFactory);
+   }).then(function(rs){
+      proxyFactory = rs;
       return proxyFactory.createProxy('0xf76fca3604e2005fe59bd59bdf97075f631fd2bc', dinstance.address);
-     
    }).then(function(logs){
-      const proxyAddress = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy
-      
+      proxyAddress = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy;
+      console.log('proxyaddress', proxyAddress);
       return proxyAddress;
-   })
-   .then(function(proxyAddress){
-      proxyaddress = proxyAddress;
+   }).then(function(proxyAddress){
       return storageinstance.addAdmin(proxyAddress);
-     
-   }).then(function(){
-      return DigitalContract.at(proxyaddress).transferOwnership('0xb10ca39dfa4903ae057e8c26e39377cfb4989551');
    }).then(function(rs){
-      console.log('re', rs.logs[0].args)
-      console.log('proxyAddress', proxyaddress)
-      return DigitalContract.at(proxyaddress).setStorage(storageaddress);
+      return DigitalContract.at(proxyAddress).transferOwnership('0xb10ca39dfa4903ae057e8c26e39377cfb4989551');
    }).then(function(rs){
-       console.log('Set admin storage to the Proxy done ' , rs.logs)
-       process.exit(1);
-  
-   }).catch(function(err){
-      console.log(err);
-       process.exit(1);
-   });
+      return DigitalContract.at(proxyAddress).setStorage(storageinstance.address);
+   })
+
 };
