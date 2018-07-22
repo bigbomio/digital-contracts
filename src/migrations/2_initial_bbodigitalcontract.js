00828c29d4a796528c5c1ca97b7df7ca2ce99eb4
chris nguyen <chuonchuonit@gmail.com>
2018-07-22 10:07:19 +0700

    add check isUniqueAddress for pendingAddresses`

--- src/migrations/2_initial_bbodigitalcontract.js
+++ src/migrations/2_initial_bbodigitalcontract.js
@@ -1,33 +1,37 @@
 const DigitalContract =  artifacts.require("BigbomDigitalContract");
 const BBStorage =  artifacts.require("BBStorage");
 const ProxyFactory = artifacts.require("UpgradeabilityProxyFactory");
+const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
 
 
 module.exports = function(deployer) {
-   
-   var storageinstance ;
-   var dinstance;
-   var proxyFactory ;
-   var proxyAddress;
-   deployer.deploy(BBStorage).then(function(rs){
-      storageinstance = rs;
-      return deployer.deploy(DigitalContract);
-   }).then(function(rs){
-      dinstance = rs;
-      return deployer.deploy(ProxyFactory);
-   }).then(function(rs){
-      proxyFactory = rs;
-      return proxyFactory.createProxy('0xf76fca3604e2005fe59bd59bdf97075f631fd2bc', dinstance.address);
-   }).then(function(logs){
-      proxyAddress = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy;
-      console.log('proxyaddress', proxyAddress);
-      return proxyAddress;
-   }).then(function(proxyAddress){
-      return storageinstance.addAdmin(proxyAddress);
-   }).then(function(rs){
-      return DigitalContract.at(proxyAddress).transferOwnership('0xb10ca39dfa4903ae057e8c26e39377cfb4989551');
-   }).then(function(rs){
-      return DigitalContract.at(proxyAddress).setStorage(storageinstance.address);
-   })
 
+   if(deployer.network_id != 777){
+      if(!DigitalContract.deployed()){
+         var storageinstance ;
+         var dinstance;
+         var proxyFactory ;
+         var proxyAddress;
+         deployer.deploy(BBStorage).then(function(rs){
+            storageinstance = rs;
+            return deployer.deploy(DigitalContract);
+         }).then(function(rs){
+            dinstance = rs;
+            return deployer.deploy(ProxyFactory);
+         }).then(function(rs){
+            proxyFactory = rs;
+            return proxyFactory.createProxy('0xf76fca3604e2005fe59bd59bdf97075f631fd2bc', dinstance.address);
+         }).then(function(logs){
+            proxyAddress = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy;
+            console.log('proxyaddress', proxyAddress);
+            return proxyAddress;
+         }).then(function(proxyAddress){
+            return storageinstance.addAdmin(proxyAddress);
+         }).then(function(rs){
+            return DigitalContract.at(proxyAddress).transferOwnership('0xb10ca39dfa4903ae057e8c26e39377cfb4989551');
+         }).then(function(rs){
+            return DigitalContract.at(proxyAddress).setStorage(storageinstance.address);
+         })
+      }
+   }  
 };
---
+++ src/migrations/3_upgrade_bbocontract.js
@@ -0,0 +1,26 @@
+const DigitalContract =  artifacts.require("BigbomDigitalContract");
+const BBStorage =  artifacts.require("BBStorage");
+const ProxyFactory = artifacts.require("UpgradeabilityProxyFactory");
+const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
+
+
+module.exports = function(deployer) {
+   if(deployer.network_id != 777){
+     if(DigitalContract.deployed()){
+
+   		var proxyAddress = '0x8f4d25f6f41568461167d62d0dd96f55ecdba58c';
+   		if(deployer.network_id == 3){
+   			proxyAddress = '0x78ffdca617a1183d01e1a508366a1a9bbba64ecb';
+   		}
+	   deployer.deploy(DigitalContract).then(function(rs){
+	      return AdminUpgradeabilityProxy.at(proxyAddress).upgradeTo(rs.address);
+	   }).then(function(rs){
+	      return AdminUpgradeabilityProxy.at(proxyAddress).implementation();
+	   }).then(function(rs){
+	      console.log('proxy implementation: ', rs);
+	   });
+     }
+	}
+};
+
+  
