c72bd6677b895228aef4d8bdaef025a2f9bf291a
Tran Tho <trantho3910@gmail.com>
2018-09-26 17:28:16 +0700

    add new script deploy

---
+++ src/migrations/5_initial_freelancer_contract.js
@@ -0,0 +1,114 @@
+const BBFreelancerJob = artifacts.require("BBFreelancerJob");
+const BBFreelancerBid = artifacts.require("BBFreelancerBid");
+const BBFreelancerPayment = artifacts.require("BBFreelancerPayment");
+const BBStorage = artifacts.require("BBStorage");
+const ProxyFactory = artifacts.require("UpgradeabilityProxyFactory");
+const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
+
+
+var accounts;
+
+
+
+module.exports = async function (deployer) {
+  // await web3.eth.getAccounts(function (err, res) {
+  //   accounts = res;
+  //  // console.log('accounts:', accounts);
+
+  // });
+  console.log('deployer.network_id ', deployer.network_id);
+  if (deployer.network_id == 3) {
+
+    var admin = '0x83e5353fc26643c29b041a3b692c6335c97a9aed';
+    var adminProxy = '0xa867a6a820928c64ffe3e30166481ec526d38bc5';
+    var BBOAddress = '0x1d893910d30edc1281d97aecfe10aefeabe0c41b';
+    var storage;
+    var jobInstance;
+    var bidInstance;
+    var paymentInstance;
+    var proxyFact;
+    var proxyAddressJob;
+    var proxyAddressBid;
+    var proxyAddressPayment;
+    var job;
+    var bid;
+    var payment;
+
+    deployer.deploy(BBStorage).then(function (rs) {
+      storage = rs;
+      return deployer.deploy(BBFreelancerJob);
+    }).then(function (rs) {
+      jobInstance = rs;
+      return deployer.deploy(BBFreelancerBid);
+    }).then(function (rs) {
+      bidInstance = rs;
+      return deployer.deploy(BBFreelancerPayment);
+    }).then(function (rs) {
+      paymentInstance = rs;
+      return deployer.deploy(ProxyFactory);
+    }).then(function (rs) {
+      proxyFact = rs;
+      return proxyFact.createProxy(adminProxy, jobInstance.address);
+    }).then(function (logs) {
+      proxyAddressJob = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy
+      console.log('proxyAddressJob', proxyAddressJob)
+      return proxyFact.createProxy(adminProxy, bidInstance.address);
+    }).then(function (logs) {
+      proxyAddressBid = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy
+      console.log('proxyAddressBid', proxyAddressBid)
+      return proxyFact.createProxy(adminProxy, paymentInstance.address);
+    }).then(function (logs) {
+      proxyAddressPayment = logs.logs.find(l => l.event === 'ProxyCreated').args.proxy
+      console.log('proxyAddressPayment', proxyAddressPayment)
+      return storage.addAdmin(proxyAddressJob, true);
+    }).then(function (rs) {
+      console.log('storage addAdmin proxyAddressJob Done');
+      return storage.addAdmin(proxyAddressBid, true);
+    }).then(function (rs) {
+      console.log('storage addAdmin proxyAddressBid Done');
+      return storage.addAdmin(proxyAddressPayment, true);
+    }).then(function (rs) {
+      console.log('storage addAdmin proxyAddressPayment Done');
+      return BBFreelancerJob.at(proxyAddressJob);
+    }).then(function (rs) {
+      job = rs;
+      console.log('job transferOwnership ....')
+      return job.transferOwnership(admin)
+    }).then(function (rs) {
+      console.log('job storage.address : ', storage.address);
+      return job.setStorage(storage.address)
+    }).then(function (rs) {
+      console.log('job setBBO ....')
+      return job.setBBO(BBOAddress)
+    }).then(function (rs) {
+      return BBFreelancerBid.at(proxyAddressBid)
+    }).then(function (rs) {
+      bid = rs;
+      console.log('bid transferOwnership ....')
+      return bid.transferOwnership(admin)
+    }).then(function (rs) {
+      console.log('bid storage.address : ', storage.address);
+      return bid.setStorage(storage.address)
+    }).then(function (rs) {
+      console.log('bid setBBO ....')
+      return bid.setBBO(BBOAddress)
+    }).then(function(rs){
+      return BBFreelancerPayment.at(proxyAddressPayment);
+    }).then(function (rs) {
+      payment = rs;
+      console.log('payment transferOwnership ....')
+      return payment.transferOwnership(admin)
+    }).then(function (rs) {
+      console.log('payment storage.address : ', storage.address);
+      return payment.setStorage(storage.address)
+    }).then(function (rs) {
+      console.log('payment setBBO ....')
+      return payment.setBBO(BBOAddress)
+    }).then(function (rs) {
+      console.log('bid setPaymentContract ....')
+      return bid.setPaymentContract(proxyAddressPayment)
+    }).then(function(rs){
+      console.log('Done')
+    })
+  }
+};
