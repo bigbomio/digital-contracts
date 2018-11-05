33d69f75d3a6fb6068e41de5d9aafd03c4a8668f
Chris TN <ksmanhtuong103@gmail.com>
2018-11-05 11:53:05 +0700

    fix bug unitest

--- src/test/freelancer.js
+++ src/test/freelancer.js
@@ -1428,7 +1428,7 @@ contract('BBFreelancer Test', async (accounts) => {
     });
     const jobHashRs1 = jobLog.logs.find(l => l.event === 'PaymentClaimed').args
     
-    const jobHashRs = jobHashRs1.jobHash
+    const jobHashRs = jobHashRs1.indexJobHash
      assert.equal(web3.utils.sha3(jobHash3), jobHashRs);
   });
 
@@ -1490,7 +1490,7 @@ contract('BBFreelancer Test', async (accounts) => {
     });
     const jobHashRs1 = jobLog.logs.find(l => l.event === 'DisputeFinalized').args
     
-    const jobHashRs = jobHashRs1.jobHash
+    const jobHashRs = jobHashRs1.indexJobHash
      assert.equal(web3.utils.sha3(jobHash4), jobHashRs);
     assert.equal(userB, jobHashRs1.winner);
 
