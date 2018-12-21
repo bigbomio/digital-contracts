7375400d5e18d24d4533d75b354bbee3262ab52b
chris nguyen <chuonchuonit@gmail.com>
2018-12-21 08:24:36 +0700

    fix unit test

--- src/migrations/12_hotfix.js
+++ src/migrations/12_hotfix.js
@@ -4,10 +4,11 @@ const BBStorage = artifacts.require("BBStorage");
 const BBFreelancerPayment = artifacts.require("BBFreelancerPayment");
 
 module.exports = async function(deployer) {
+	if (deployer.network_id == 3) {
 	return deployer.then(async function(){
-		await BBStorage.at('0xb83d8faa19b3bd03d200e4eb9f985ac247497726').addAdmin('0xda452218a3c7521f7de4aa1a1e9411998d77e468', true);
+		//await BBStorage.at('0xb83d8faa19b3bd03d200e4eb9f985ac247497726').addAdmin('0xda452218a3c7521f7de4aa1a1e9411998d77e468', true);
 		// await BBFreelancerPayment.at('0xf5cf17e2059b78ca9012475309c296c4e6c8a79c').addToken('0x1d893910d30edc1281d97aecfe10aefeabe0c41b', true);
-		// await BBFreelancerPayment.at('0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebb0').addToken('0x1d893910d30edc1281d97aecfe10aefeabe0c41b', true);
+		 await BBFreelancerPayment.at('0xf5cf17e2059b78ca9012475309c296c4e6c8a79c').addToken('0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeebb0', true);
 		
 	});
   //let BBUnOrderedTCR = '0x2f700e71786b95d3469a27073974689a5772fa8b'
@@ -17,4 +18,5 @@ module.exports = async function(deployer) {
   // console.log(a);
   // let b = await votingI.claimReward('QmbhX9KkRYtoHAUPCyrYprtAp46xvfs8ZPqJHoDw15jg16');
 //  return deployer.deploy(BBDispute);
+}
 }
